2de274a4f2f33e3ec55730ac9fe245cc34d3fcbc
phuc <nguyenphuc.vna@gmail.com>
2021-07-05 20:09:15 +0700

    added admin pagination

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,44 +1,87 @@
 
-let article;
-let pageNumber =1;
-let apiUrl = 'http://localhost:8080/api/articles/?pageNumber';
-
-async function getJson(apiUrl){
-  let response = await fetch(apiUrl+pageNumber);
-  article = await response.json();
-  console.log(article);
-  return article;
-}
-
-function appendTable(){
-  getJson(apiUrl).then(()=>{
-    const articleTable = document.querySelector('table');
-    let length = article.articles.length;
-    console.log(length);
-    for(let i=0; i<length;i++){
-      let articleTableBodyRow = document.createElement('tr');
-      articleTableBodyRow.className = 'articleTableBodyRow';
-      let title = document.createElement('td');
-      title.scope = 'row';
-      title.innerText = article.articles[i].title;
-
-      let body = document.createElement('td');
-      body.scope = 'row';
-      body.innerText = article.articles[i].body;
-
-      let tag = document.createElement('td');
-      tag.scope = 'row';
-      let tagLen = article.articles[i].tags.length;
-      console.log(tagLen);
-      for(let j =0;j<tagLen;j++){
-        tag.innerText +=  article.articles[i].tags[j].name +' ';
-      }
-      articleTableBodyRow.append(title,body,tag);
-      articleTable.append(articleTableBodyRow);
-    }
-    
+let urlParam =  new URLSearchParams();
+let current = urlParam.get('page');
+const board = document.querySelector('.table');
+const boardTBody = board.querySelector('.article-table-body');
+const pageLink = document.querySelector('.article-paging');
+function showPage(){ 
+  getPage().then((result)=>{
+    showTable(result);
+    showNavigation(result);
   })
 }
-appendTable();
 
+async function getPage() {
+  
+  let response = await fetch(`http://localhost:8080/api/articles/?`+urlParam);
+  let data = await response.json()
+  //console.log(data);
+  return data;
+}
+
+function showTable(result){
+  
+  //console.log(board); 
+  for(let i=0;i<result.articles.length;i++){
+    
+    const boardBodyRow = document.createElement('tr');
+    boardBodyRow.className = 'boardBodyRow';
+
+    const boardBodyId = document.createElement('td');
+    boardBodyId.innerHTML = result.articles[i].id;
+    
+    const boardBodyTitle = document.createElement('td');
+    boardBodyTitle.innerHTML =result.articles[i].title;
+
+    const boardBodyBody = document.createElement('td');
+    boardBodyBody.innerHTML = result.articles[i].body;
+
+    const boardBodyTag = document.createElement('td');
+    for(let j=0;j<result.articles[i].tags.length;j++){
+      boardBodyTag.innerHTML = result.articles[i].tags[j].name;
+    }
+    boardBodyRow.append(boardBodyId,boardBodyTitle,boardBodyBody,boardBodyTag);
+    boardTBody.append(boardBodyRow);
+    board.append(boardTBody);
+  } 
+}
+function showNavigation(result){
+  
+  if(current ==null){
+    current =1;
+  }
+  if(current >1){
+    const prevLink = document.createElement('a');
+    prevLink.title = 'go to previous page';
+    prevLink.href ='javascript:showPage()';
+    prevLink.innerText = 'Prev ';
+    prevLink.addEventListener('click',()=>{
+      current = Number(current-1);
+      setStateAndEmptyPage();
+    });
+    pageLink.append(prevLink);
+  }
+  if(current<result.totalPages){
+    const nextLink = document.createElement('a');
+
+    nextLink.title = 'go to next page';
+    nextLink.href ='javascript:showPage()';
+    nextLink.innerText = 'Next';
+    nextLink.addEventListener('click',()=>{
+      current = Number(current+1);      
+      setStateAndEmptyPage();
+    });
+    pageLink.append(nextLink);
+  }
+}
+
+function setStateAndEmptyPage(){
+  urlParam.set('page',current);
+  window.history.replaceState('','','?'+urlParam.toString());
+  boardTBody.innerHTML ='';
+  pageLink.innerHTML ='';
+}
+
+
+showPage();
 
