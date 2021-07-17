a85135d094973493f70bed4263143baadebfb2a2
phuc <nguyenphuc.vna@gmail.com>
2021-07-17 14:38:05 +0700

    add user page

---
+++ user/js/article.js
@@ -0,0 +1,28 @@
+let idUrl = location.search == "" ? " " : Number(location.search.substr(1));
+console.log(idUrl);
+if (!Number.isInteger(idUrl)) {
+    idUrl = 1;
+}
+
+
+async function getData() {
+    let res = await fetch("http://localhost:8080/api/article/" + Number(idUrl));
+    let result = await res.json();
+    console.log(result);
+    return result;
+}
+
+function display() {
+    getData().then(result => {
+        getArticle(result);
+    })
+}
+function getArticle(result) {
+    let title = document.querySelector('.title');
+    console.log(title);
+    title.innerHTML = result.title;
+    let content = document.querySelector('.content');
+    console.log(content);
+    content.innerHTML = result.body;
+}
+display();
